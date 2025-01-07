2990ef0c8d13182b5e97f1ae5ece220dad6480d0
anandaputri01 <anandacantic@gmail.com>
2025-01-07 12:46:26 +0700

    Pesan commit

--- admin.php
+++ admin.php
@@ -57,9 +57,6 @@ if (!isset($_SESSION['username'])) {
             <li class="nav-item">
                 <a class="nav-link" href="admin.php?page=article">Collection</a>
             </li> 
-                <li class="nav-item">
-                <a class="nav-link" href="admin.php?page=gallery">Gallery</a>
-            </li> 
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle text-warning fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <?= $_SESSION['username']?>
